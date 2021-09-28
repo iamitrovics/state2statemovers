91da3421da9235709a65b30b2dc9d0d235838fba
Isidora Kalicanin <isidorakalicanin@madeonline.agency>
2021-09-28 17:17:11 +0200

    author info update

--- single.php
+++ single.php
@@ -29,9 +29,16 @@ $container = get_theme_mod( 'understrap_container_type' );
                     $categories = get_the_category($post->ID);
                     $cat_link = get_category_link($categories[0]->cat_ID);
                     echo '<a href="'.$cat_link.'">'.$categories[0]->cat_name.'</a>' 
-                    ?>   /                            
-                    By <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a>
+                    ?>
                 </span>
+                <div class="author-desc">
+                    <?php echo get_avatar( get_the_author_meta( 'ID' ), 60 ); ?>
+                    <div class="author-content">
+                        <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a>
+                        <p><?php the_author_description(); ?></p>
+                    </div>
+                    <!-- /.author-content -->
+                </div>
             </div>
             <!-- /.blog-meta -->
         </div>
@@ -177,44 +184,7 @@ $container = get_theme_mod( 'understrap_container_type' );
                 <!-- /.next blog-nav-item -->  
             </div>
             <!-- /.blog-navigation -->
-            <div class="related-posts">
-                <span class="related-posts-title">Realted posts</span>
-                <!-- /.related-posts-title -->
-
-                    <?php $orig_post = $post;
-                    global $post;
-                    $categories = get_the_category($post->ID);
-                    if ($categories) {
-                    $category_ids = array();
-                    foreach($categories as $individual_category) $category_ids[] = $individual_category->term_id;
-
-                    $args=array(
-                    'category__in' => $category_ids,
-                    'post__not_in' => array($post->ID),
-                    'posts_per_page'=> 2, // Number of related posts that will be shown.
-                    'ignore_sticky_posts'=>1
-                    );
-
-                    $my_query = new wp_query( $args );
-                    if( $my_query->have_posts() ) {
-                    while( $my_query->have_posts() ) {
-                    $my_query->the_post();?>
-
-                    <div class="related-box">
-                        <h4><a href="<?php get_permalink(); ?>"><?php the_title(); ?></a></h4>
-                    </div>
-                    <!-- /.related-box -->
-
-                    <?
-                    }
-                    echo '</ul></div>';
-                    }
-                    }
-                    $post = $orig_post;
-                    wp_reset_query(); ?>
-
-            </div>
-            <!-- /.related-posts -->
+            
         </div>
         <!-- /.blog-body -->
     </div>
