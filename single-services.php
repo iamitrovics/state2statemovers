79a89b46f1aaafafdee3bc0270e565575b4e2f2c
Isidora Kalicanin <isidorakalicanin@madeonline.agency>
2021-12-09 15:52:37 +0100

    services page update

--- single-services.php
+++ single-services.php
@@ -33,90 +33,140 @@ $container = get_theme_mod( 'understrap_container_type' );
     </div>
     <!-- /#inner-header -->
     <div class="inner-page" id="services-page">
-        
-        <div class="container">
-            <div class="row">
-                <div class="col-md-10 offset-md-1">
-                <div class="inner-page-in">
+        <div class="inner-page-in">
 
-            <?php if( have_rows('content_layout_regular') ): ?>
-                <?php while( have_rows('content_layout_regular') ): the_row(); ?>
+                <div class="row">
+                    <div class="col-lg-9 city-content-wrap">
 
-                    <?php if( get_row_layout() == 'full_width_content' ): ?>
-                    
-                        <?php the_sub_field('content_block'); ?>
+                        <?php if( have_rows('content_layout_regular') ): ?>
+                            <?php while( have_rows('content_layout_regular') ): the_row(); ?>
 
-                    <?php elseif( get_row_layout() == 'full_width_image' ): ?>
-                        <div class="image-holder">
-                            <?php
-                            $imageID = get_sub_field('featured_image');
-                            $image = wp_get_attachment_image_src( $imageID, 'big-image' );
-                            $alt_text = get_post_meta($imageID , '_wp_attachment_image_alt', true);
-                            ?> 
+                                <?php if( get_row_layout() == 'full_width_content' ): ?>
+                                
+                                    <?php the_sub_field('content_block'); ?>
 
-                            <img class="img-responsive" alt="<?php echo $alt_text; ?>" src="<?php echo $image[0]; ?>" />                         
-                        </div>
-                    <?php elseif( get_row_layout() == 'gallery' ): ?>
-                        
-						<div class="row">
+                                <?php elseif( get_row_layout() == 'full_width_image' ): ?>
+                                    <div class="image-holder">
+                                        <?php
+                                        $imageID = get_sub_field('featured_image');
+                                        $image = wp_get_attachment_image_src( $imageID, 'big-image' );
+                                        $alt_text = get_post_meta($imageID , '_wp_attachment_image_alt', true);
+                                        ?> 
 
-							<?php 
-							$images = get_sub_field('photo_gallery');
-							if( $images ): ?>
-								<?php foreach( $images as $image ): ?>
-									<div class="col-md-3">
-										<img src="<?php echo $image['sizes']['thumb-image']; ?>" class="img-responsive" alt="<?php echo $image['alt']; ?>" />
-									</div>
-									<!-- /.col-md-3 -->
-								<?php endforeach; ?>
-							<?php endif; ?>                
+                                        <img class="img-responsive" alt="<?php echo $alt_text; ?>" src="<?php echo $image[0]; ?>" />                         
+                                    </div>
+                                <?php elseif( get_row_layout() == 'gallery' ): ?>
+                                    
+                                    <div class="row">
 
-						</div>
-						<!-- /.row -->
+                                        <?php 
+                                        $images = get_sub_field('photo_gallery');
+                                        if( $images ): ?>
+                                            <?php foreach( $images as $image ): ?>
+                                                <div class="col-md-3">
+                                                    <img src="<?php echo $image['sizes']['thumb-image']; ?>" class="img-responsive" alt="<?php echo $image['alt']; ?>" />
+                                                </div>
+                                                <!-- /.col-md-3 -->
+                                            <?php endforeach; ?>
+                                        <?php endif; ?>                
 
-                        <?php elseif( get_row_layout() == 'video' ): ?>
+                                    </div>
+                                    <!-- /.row -->
 
-                            <div class="blog-video">
-                                <?php the_sub_field('embedded_code'); ?>
-                            </div>
-                            <!-- // video  -->
+                                    <?php elseif( get_row_layout() == 'video' ): ?>
 
-                        <?php elseif( get_row_layout() == 'accordion' ): ?>
-
-                            <div class="default-accordion blog__acc">
-                                <h3><?php the_sub_field('accordion_title'); ?></h3>
-                                <?php if( have_rows('accordion_list') ): ?>
-                                    <?php while( have_rows('accordion_list') ): the_row(); ?>
-
-                                        <div class="faq-box">
-                                            <h4><?php the_sub_field('heading'); ?></h4>
-
-                                            <div>
-                                                <?php the_sub_field('content'); ?>
-                                                <!-- /.faq-box -->
-                                            </div>
-                                            <!-- /.faq-box -->
+                                        <div class="blog-video">
+                                            <?php the_sub_field('embedded_code'); ?>
                                         </div>
+                                        <!-- // video  -->
 
-                                    <?php endwhile; ?>
+                                    <?php elseif( get_row_layout() == 'image_left_text_right' ): ?>
+
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="image-holder">
+                                                    <?php
+                                                    $imageID = get_sub_field('featured_image');
+                                                    $image = wp_get_attachment_image_src( $imageID, 'big-image' );
+                                                    $alt_text = get_post_meta($imageID , '_wp_attachment_image_alt', true);
+                                                    ?> 
+
+                                                    <img class="img-responsive" alt="<?php echo $alt_text; ?>" src="<?php echo $image[0]; ?>" />                         
+                                                </div>
+                                            </div> <!-- col-md-6 -->
+                                            <div class="col-md-6">
+                                                <?php the_sub_field('content_block'); ?>
+                                            </div> <!-- col-md-6 -->
+                                        </div>
+                                        <!-- /.row -->   
+
+                                    <?php elseif( get_row_layout() == 'image_right_text_left' ): ?>
+
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <?php the_sub_field('content_block'); ?>
+                                            </div> <!-- col-md-6 -->
+                                            <div class="col-md-6">
+                                                <div class="image-holder">
+                                                    <?php
+                                                    $imageID = get_sub_field('featured_image');
+                                                    $image = wp_get_attachment_image_src( $imageID, 'big-image' );
+                                                    $alt_text = get_post_meta($imageID , '_wp_attachment_image_alt', true);
+                                                    ?> 
+
+                                                    <img class="img-responsive" alt="<?php echo $alt_text; ?>" src="<?php echo $image[0]; ?>" />                         
+                                                </div>
+                                            </div> <!-- col-md-6 -->
+                                        </div>
+                                        <!-- /.row -->   
+
+                                    <?php elseif( get_row_layout() == 'accordion' ): ?>
+
+                                        <div class="default-accordion blog__acc">
+                                            <h3><?php the_sub_field('accordion_title'); ?></h3>
+                                            <?php if( have_rows('accordion_list') ): ?>
+                                                <?php while( have_rows('accordion_list') ): the_row(); ?>
+
+                                                    <div class="faq-box">
+                                                        <h4><?php the_sub_field('heading'); ?></h4>
+
+                                                        <div>
+                                                            <?php the_sub_field('content'); ?>
+                                                            <!-- /.faq-box -->
+                                                        </div>
+                                                        <!-- /.faq-box -->
+                                                    </div>
+
+                                                <?php endwhile; ?>
+                                            <?php endif; ?>
+                                        </div>
+                                        <!-- /.default-accordion -->  
+
                                 <?php endif; ?>
+                            <?php endwhile; ?>
+                        <?php endif; ?>
+
+                        <div id="masheader-content">
+                            <div id="bottom-form">
+                                <?php include (TEMPLATEPATH . '/inc/inc_quote_form.php' ); ?>
                             </div>
-                            <!-- /.default-accordion -->  
+                        </div>
+                        <!-- // contentn  -->
+                    </div>
+                    <!-- /.col-lg-9 -->
+                    <div class="col-lg-3">
+                        <div class="city-form">
+                            <h2 class="services-page-heading"><?php the_field('main_title_sidebar_form', 'options'); ?></h2>
+                            <?php the_field('form_code_sidebar', 'options'); ?>
+                        </div>
+                        <!-- /.city-form -->
+                    </div>
+                    <!-- /.col-lg-3 -->
+                </div>
+                <!-- /.row -->
 
-                    <?php endif; ?>
-                <?php endwhile; ?>
-            <?php endif; ?>
-            </div>
-        <!-- /.inner-page-in -->
-
-            </div>
-                <!-- /.col-md-12 -->
-            </div>
-            <!-- /.row -->
         </div>
-        <!-- /.container -->
-
-        
+        <!-- /.inner-page-in -->
     </div>
     <!-- /.inner-page -->
 
