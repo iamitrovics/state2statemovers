394fface6e65ca6611ed5015223a07c6a34d3f99
Srdjan Mitrovic <iam@mitrovics.com>
2022-06-13 21:27:23 +0200

    fix admin search

--- functions.php
+++ functions.php
@@ -74,18 +74,21 @@ function init_remove_support(){
     remove_post_type_support( $post_type, 'editor');
 }
 
+if ( ! is_admin() ) {
 
-function fb_filter_query( $query, $error = true ) {
+	function fb_filter_query( $query, $error = true ) {
 
-    if ( is_search() ) {
-        $query->is_search = false;
-        $query->query_vars['s'] = false;
-        $query->query['s'] = false;
+		if ( is_search() ) {
+			$query->is_search = false;
+			$query->query_vars['s'] = false;
+			$query->query['s'] = false;
 
-        if ( $error == true )
-            $query->is_404 = true;
-    }
+			if ( $error == true )
+				$query->is_404 = true;
+		}
+	}
+
+	add_action( 'parse_query', 'fb_filter_query' );
+	add_filter( 'get_search_form', function() { return null;} );
+
 }
-
-add_action( 'parse_query', 'fb_filter_query' );
-add_filter( 'get_search_form', function() { return null;} );
